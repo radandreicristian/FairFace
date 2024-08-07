fdc4bf546ec4a189ae1851b87eac9de9cb6a5c8b
Andrei-Cristian Rad <andrei.rad@connatix.com>
2024-08-07 15:50:31 +0300

    fix: use cpu when cuda is not available

--- predict.py
+++ predict.py
@@ -63,13 +63,20 @@ def predidct_age_gender_race(save_prediction_at, imgs_path = 'cropped_faces/'):
 
     model_fair_7 = torchvision.models.resnet34(pretrained=True)
     model_fair_7.fc = nn.Linear(model_fair_7.fc.in_features, 18)
-    model_fair_7.load_state_dict(torch.load('dlib_models/res34_fair_align_multi_7_20190809.pt'))
+    if torch.cuda.is_available():
+        model_fair_7.load_state_dict(torch.load('dlib_models/res34_fair_align_multi_7_20190809.pt'))
+    else:
+        model_fair_7.load_state_dict(torch.load('dlib_models/res34_fair_align_multi_7_20190809.pt',  map_location=torch.device('cpu')))
     model_fair_7 = model_fair_7.to(device)
     model_fair_7.eval()
 
     model_fair_4 = torchvision.models.resnet34(pretrained=True)
     model_fair_4.fc = nn.Linear(model_fair_4.fc.in_features, 18)
-    model_fair_4.load_state_dict(torch.load('dlib_models/res34_fair_align_multi_4_20190809.pt'))
+    if torch.cuda.is_available:
+        model_fair_4.load_state_dict(torch.load('dlib_models/res34_fair_align_multi_4_20190809.pt'))
+    else:
+        model_fair_4.load_state_dict(torch.load('dlib_models/res34_fair_align_multi_4_20190809.pt', map_location=torch.device('cpu')))
+
     model_fair_4 = model_fair_4.to(device)
     model_fair_4.eval()
 
@@ -206,7 +213,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--csv', dest='input_csv', action='store',
                         help='csv file of image path where col name for image path is "img_path')
-    dlib.DLIB_USE_CUDA = True
+
+    dlib.DLIB_USE_CUDA = torch.cuda.is_available
+
     print("using CUDA?: %s" % dlib.DLIB_USE_CUDA)
     args = parser.parse_args()
     SAVE_DETECTED_AT = "detected_faces"
